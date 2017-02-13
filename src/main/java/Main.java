f588a5c8ec08ce6b04cce8795d36138b946e011a
Anna Ilchenko <mymihtav@gmail.com>
2017-02-12 16:35:46 -0800

    demo of prototype scope

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,9 +17,18 @@ public class Main {
 		Game game = context.getBean("baseballGame", Game.class);
 		Game game2 = context.getBean("baseballGame", Game.class);
 		
+		/**
+		 * here game and game2 are two separate objects
+		 * because we used @Scope("prototype") on BaseballGame class
+		 * this forces Spring create a new instance every time we need a new reference
+		 */
 		System.out.println(game2);
 		game.setAwayTeam(context.getBean("redSox",Team.class));
 		System.out.println(game2);
+		System.out.println(game);
+		
+		
+		
 
 
 	}
